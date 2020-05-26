19d64d4f00c94919aadee5fb2019e51e3d557312
Shiro Han <shan99business@gmail.com>
2020-05-26 22:12:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(in_key, in_value, output)|
+    binding.pry
+  end
+  binding.pry
 end
+
+=begin
+data.each_with_object({}) do |(key1, value1), output|
+  value.each do |key2, names|
+    names.each do |name|
+      if !output[name]
+        output[name] = {}
+      end
+      if !output[name][key1]
+        !output[name][key1] = []
+      end
+      output[name][key].push(key2)
+    end
+  end
+  output
+end
+=end
